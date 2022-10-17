8314bc9af15c020a430c50d0042ee6fbba98f8c8
koss054 <konstantin054@abv.bg>
2022-10-17 19:46:30 +0300

    Upload: Assignment - Data Types
    The problem for the lecture Data Types.

--- 01.Fundamentals - Part 1/assignment.js
+++ 01.Fundamentals - Part 1/assignment.js
@@ -1,3 +1,4 @@
+// Assignment - Values and Variables
 let country = "Bulgaria";
 let continent = "Europe";
 let population = 7000000;
@@ -5,3 +6,11 @@ let population = 7000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Assignment - Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
