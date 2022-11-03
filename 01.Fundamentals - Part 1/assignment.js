b1d11e98592946a092a9189c4a717326363f8425
koss054 <konstantin054@abv.bg>
2022-11-03 11:39:53 +0200

    Complete assignment for type conversion and coercion

--- 01.Fundamentals - Part 1/assignment.js
+++ 01.Fundamentals - Part 1/assignment.js
@@ -65,3 +65,10 @@ population = 130000000;
 isCountryPopulationAboveAverage(population, averageCountryPopulation, country);
 
 population = populationBeforeTempChange;
+
+// Assignment - Type Conversion and Coercion          *guessing the output
+console.log('9' - '5');                               //4
+console.log('19' - '13' + '17');                      //617
+console.log('19' - '13' + 17);                        //23
+console.log('123' < 57);                              //false
+console.log(5 + 6 + '4' + 9 - 4 - 2);                 //1143
