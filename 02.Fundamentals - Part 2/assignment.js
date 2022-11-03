994ee5dc5a0afa964209d4d3de84c7647b55a45d
koss054 <konstantin054@abv.bg>
2022-11-03 19:27:38 +0200

    Complete first part of the assignment for function declaration vs expression

--- 02.Fundamentals - Part 2/assignment.js
+++ 02.Fundamentals - Part 2/assignment.js
@@ -10,3 +10,20 @@ const germany = describeCountry("Germany", 83000000, "Berlin");
 console.log(bulgaria);
 console.log(romania);
 console.log(germany);
+
+// Assignment - Function Declarations vs. Expressions
+const worldPopulation = 7_900_000_000;
+
+function percentageOfWorld1(population, countryName) {
+    const populationPercentage = ((population / worldPopulation) * 100).toFixed(1);
+    const populationForString = population / 1_000_000;
+    return `${countryName} has ${populationForString} million people, so it's about ${populationPercentage}% of the world population`;
+}
+
+const china = percentageOfWorld1(1_441_000_000, "China");
+const brazil = percentageOfWorld1(212_000_000, "Brazil");
+const portugal = percentageOfWorld1(10_000_000, "Portugal");
+
+console.log(china);
+console.log(brazil);
+console.log(portugal);
