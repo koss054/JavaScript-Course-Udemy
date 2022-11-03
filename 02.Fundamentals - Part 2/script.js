1ea96dcf5d3f296fcc37d5d06f3f099134216a01
koss054 <konstantin054@abv.bg>
2022-11-03 20:08:49 +0200

    Complete coding challenge #1 - section 3

---
+++ 02.Fundamentals - Part 2/script.js
@@ -0,0 +1,19 @@
+// ********************
+// Coding Challenge #1
+// ********************
+const calcAverage = (firstScore, secondScore, thirdScore) => (firstScore + secondScore + thirdScore) / 3;
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins > avgKoalas * 2) {
+        console.log(`Dolphins win (D: ${avgDolphins} vs. K: ${avgKoalas})`);
+    } else if (avgKoalas > avgDolphins * 2) {
+        console.log(`Koalas win (K: ${avgKoalas} vs. D: ${avgDolphins})`);
+    }
+}
+
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+checkWinner(avgDolphins, avgKoalas);
+
+avgDolphins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+checkWinner(avgDolphins, avgKoalas);
