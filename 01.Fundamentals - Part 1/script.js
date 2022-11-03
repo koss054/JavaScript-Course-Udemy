4f0f3327312fb1b747a9445303c7bd330cae63a4
koss054 <konstantin054@abv.bg>
2022-11-03 10:18:07 +0200

    Add test data for Coding Challenge #1

--- 01.Fundamentals - Part 1/script.js
+++ 01.Fundamentals - Part 1/script.js
@@ -15,3 +15,20 @@ function isFirstBMIHigher(firstMass, firstHeight, secondMass, secondHeight) {
     return firstBMI > secondBMI;
 }
 
+// Test Data 1
+let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let johnHeight = 1.95;
+
+let markHigherBMI = isFirstBMIHigher(markMass, markHeight, johnMass, johnHeight);
+console.log(markHigherBMI);
+
+// Test Data 2
+markMass = 95;
+markHeight = 1.88;
+johnMass = 85;
+johnHeight = 1.76;
+
+markHigherBMI = isFirstBMIHigher(markMass, markHeight, johnMass, johnHeight);
+console.log(markHigherBMI);
