ca520f0645d817dc223c2d8b8994b2c7a1944792
koss054 <konstantin054@abv.bg>
2022-11-03 10:48:52 +0200

    Add if else statement for Coding Challenge #2 and update Coding Challenge #1

--- 01.Fundamentals - Part 1/script.js
+++ 01.Fundamentals - Part 1/script.js
@@ -8,32 +8,47 @@
 // ********************
 // Coding CHallenge #1
 // ********************
-function isFirstBMIHigher(firstMass, firstHeight, secondMass, secondHeight) {
-    let firstBMI = firstMass / (firstHeight * firstHeight);
-    let secondBMI = secondMass / (secondHeight * secondHeight);
-
+function isFirstBMIHigher(firstBMI, secondBMI) {
     return firstBMI > secondBMI;
+}
+
+function calculateBMI(mass, height) {
+    return (mass / height ** 2).toFixed(1);
 }
 
 // Test Data 1
 let markMass = 78;
 let markHeight = 1.69;
+let markBMI = calculateBMI(markMass, markHeight);
+
 let johnMass = 92;
 let johnHeight = 1.95;
+let johnBMI = calculateBMI(johnMass, johnHeight);
 
-let firstCheckBMI = isFirstBMIHigher(markMass, markHeight, johnMass, johnHeight);
+let firstCheckBMI = isFirstBMIHigher(markBMI, johnBMI);
 console.log(firstCheckBMI);
+checkMarkAndJohnBMI(firstCheckBMI);
 
 // Test Data 2
 markMass = 95;
 markHeight = 1.88;
+markBMI = calculateBMI(markMass, markHeight);
+
 johnMass = 85;
 johnHeight = 1.76;
+johnBMI = calculateBMI(johnMass, johnHeight);
 
-let secondCheckBMI = isFirstBMIHigher(markMass, markHeight, johnMass, johnHeight);
+let secondCheckBMI = isFirstBMIHigher(markBMI, johnBMI);
 console.log(secondCheckBMI);
+checkMarkAndJohnBMI(secondCheckBMI);
 
 // ********************
 // Coding CHallenge #2
 // ********************
-
+function checkMarkAndJohnBMI(isMarkHigher) {
+    if (isMarkHigher) {
+        console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+    } else {
+        console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+    }
+}
