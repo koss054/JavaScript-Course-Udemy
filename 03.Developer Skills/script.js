a65570ed0e25205a69b1ba488d597ddd526fa53b
koss054 <konstantin054@abv.bg>
2022-11-04 18:27:58 +0200

    Complete coding challenge #1 - section 5

--- 03.Developer Skills/script.js
+++ 03.Developer Skills/script.js
@@ -1,3 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// ********************
+// Coding Challenge #1
+// ********************
+function printForecast(arr) {
+  let displayText = "... ";
+
+  for (let i = 0; i < arr.length; i++) {
+    displayText += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+
+  return displayText;
+}
+
+console.log(printForecast([17, 21, 23]));
+console.log(printForecast([12, 5, -5, 0, 4]));
