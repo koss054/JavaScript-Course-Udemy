c8456d5bd79d93c61a631d9137b02c2f947230fe
koss054 <konstantin054@abv.bg>
2022-11-04 12:32:13 +0200

    Complete assignment for the for loop lecture

--- 02.Fundamentals - Part 2/assignment.js
+++ 02.Fundamentals - Part 2/assignment.js
@@ -147,3 +147,8 @@ const myCountry = {
 
 console.log(myCountry.describe());
 console.log(myCountry.checkIsland());
+
+// Assignment - The for Loop
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting!`);
+}
