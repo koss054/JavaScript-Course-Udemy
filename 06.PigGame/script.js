b159e156f75b6b19d6e32ed024f759f22ae26009
Konstantin Kostadinov <konstantin054@abv.bg>
2022-12-22 12:38:53 +0200

    Add initial element selectors to PigGame

--- 06.PigGame/script.js
+++ 06.PigGame/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Selecting DOM elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let player1 = {
+    score: score0El.textContent,
+};
+
+let player2 = {
+    score: score1El.textContent,
+}
