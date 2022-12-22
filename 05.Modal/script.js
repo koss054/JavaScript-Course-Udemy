73d46c376d717359c32c4b90aea81efa578a36c2
Konstantin Kostadinov <konstantin054@abv.bg>
2022-12-22 12:10:12 +0200

    Add modal open/close functionality

--- 05.Modal/script.js
+++ 05.Modal/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+    btnOpenModal[i].addEventListener('click', function() {
+        showModal();
+    });
+}
+
+btnCloseModal.addEventListener('click', function() {
+    hideModal();
+})
+
+overlay.addEventListener('click', function() {
+    hideModal();
+})
+
+function showModal() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function hideModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
